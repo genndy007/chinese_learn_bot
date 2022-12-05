6774f7562fae65638ea0bffc98afd19e5fcdd010
genndy007 <kochevgenazp@gmail.com>
2022-12-05 18:19:00 +0200

    Create basic echo bot

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from loguru import logger
 
 from src.constants import BOT_TOKEN
 
-print(BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=["start", "help"])
+async def main(message: types.Message):
+    await message.reply("Start or help from Chinese Learn Bot")
 
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Dunno what to say")
+
+
+if __name__ == "__main__":
+    logger.info("Bot started!")
+    executor.start_polling(dp, skip_updates=True)
