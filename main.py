64269f92897f12b9fc1927c8051666207a78c8ed
genndy007 <kochevgenazp@gmail.com>
2022-12-06 16:50:53 +0200

    Rewrite adding to dispatcher with dict

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types, executor
 from loguru import logger
 
 from src.constants import BOT_TOKEN
-from src.commands import cmd_start
+from src.commands import CMD_NAME_TO_FUNC
 from src.uvloop import run_uvloop
 
 
@@ -13,7 +13,9 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-dp.register_message_handler(cmd_start, commands="start")
+for cmd_name, cmd_func in CMD_NAME_TO_FUNC.items():
+    dp.register_message_handler(cmd_func, commands=cmd_name)
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
--- src/commands.py
+++ src/commands.py
@@ -4,3 +4,23 @@ from aiogram import types
 async def cmd_start(message: types.Message):
     full_name = message.from_user.full_name
     await message.answer(f"Thanks for using bot, {full_name}")
+
+
+async def cmd_word_groups(message: types.Message):
+    await message.answer("Here are all your groups")
+
+
+async def cmd_add_group(message: types.Message):
+    await message.answer("Adding new group")
+
+
+async def cmd_add_word(message: types.Message):
+    await message.answer("Adding new word")
+
+
+CMD_NAME_TO_FUNC = {
+    "start": cmd_start,
+    "word_groups": cmd_word_groups,
+    "add_group": cmd_add_group,
+    "add_word": cmd_add_word,
+}
